f90a248db0b83721ed4a7c0e0fa98a21d033a1f6
Chris Butler <christopher_g_butler@hotmail.co.uk>
2019-03-19 21:25:58 +0000

    starting to screen scrape

--- HopkinsHomes/ScreenScrapeFunction.cs
+++ HopkinsHomes/ScreenScrapeFunction.cs
@@ -1,4 +1,5 @@
 using System;
+using HtmlAgilityPack;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
 
@@ -9,6 +10,10 @@ namespace HopkinsHomes
         [FunctionName("ScreenScrapeFunction")]
         public static void Run([TimerTrigger("0 */5 * * * *")]TimerInfo myTimer, ILogger log)
         {
+            string url = "http://html-agility-pack.net/";
+            HtmlWeb web = new HtmlWeb();
+            HtmlDocument doc = web.Load(url);
+            doc.DocumentNode.SelectNodes("table");
             string apiKey = Environment.GetEnvironmentVariable("SEND_GRID_KEY");
             log.LogInformation($"apiKey = {apiKey}");
             log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
