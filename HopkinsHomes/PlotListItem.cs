36671399514525122c68f12c7a26cc559d8f531a
Chris Butler <christopher_g_butler@hotmail.co.uk>
2019-03-21 07:34:08 +0000

    Able to plot list items from website

---
+++ HopkinsHomes/PlotListItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace HopkinsHomes
+{
+    class PlotListItem
+    {
+        public int Id { get; set; }
+        public string HouseType { get; set; }
+        public string Beds { get; set; }
+        public string Garaging { get; set; }
+        public string Status { get; set; }
+        public int Value => int.TryParse(new string(Status.Where(char.IsDigit).ToArray()), out int result) ? result : -1;
+    }
+}
--- HopkinsHomes/ScreenScrapeFunction.cs
+++ HopkinsHomes/ScreenScrapeFunction.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using HtmlAgilityPack;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
@@ -10,13 +12,36 @@ namespace HopkinsHomes
         [FunctionName("ScreenScrapeFunction")]
         public static void Run([TimerTrigger("0 */5 * * * *")]TimerInfo myTimer, ILogger log)
         {
-            string url = "http://html-agility-pack.net/";
-            HtmlWeb web = new HtmlWeb();
-            HtmlDocument doc = web.Load(url);
-            doc.DocumentNode.SelectNodes("table");
+            List<PlotListItem> displayedPlotListItems = GetPlotListItemsFromWebsite().ToList();
+
             string apiKey = Environment.GetEnvironmentVariable("SEND_GRID_KEY");
             log.LogInformation($"apiKey = {apiKey}");
             log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+        }
+
+        private static IEnumerable<PlotListItem> GetPlotListItemsFromWebsite()
+        {
+            string url = "https://www.hopkinshomes.co.uk/development/StJamesPark_ELY";
+            HtmlWeb web = new HtmlWeb();
+            HtmlDocument doc = web.Load(url);
+
+            List<HtmlNode> trNodes = doc.DocumentNode.SelectNodes("//*[@id='inner-wrapper']/div[1]/div[2]/table/tr").ToList();
+            trNodes.RemoveRange(0, 2);
+            trNodes.RemoveRange(trNodes.Count - 2, 2);
+            IEnumerable<PlotListItem> plotListItems = trNodes.Select(trNode =>
+            {
+                List<HtmlNode> tdNodes = trNode.Elements("td").ToList();
+                return new PlotListItem
+                {
+                    Id = int.TryParse(tdNodes[0].InnerHtml, out int plotId) ? plotId : -1,
+                    HouseType = tdNodes[1].InnerHtml,
+                    Beds = tdNodes[2].InnerHtml,
+                    Garaging = tdNodes[3].InnerHtml,
+                    Status = tdNodes[4].InnerHtml
+                };
+            });
+
+            return plotListItems;
         }
     }
 }
